94ec8c415ec5a2cec505b45e655887bc7d4493bc
Vic Hargrave <vichargrave@gmail.com>
2018-07-29 22:52:19 -0700

    Remove morphline ID argument

--- basicparser/src/main/java/io/github/vichargrave/basicparser/ParserApp.java
+++ basicparser/src/main/java/io/github/vichargrave/basicparser/ParserApp.java
@@ -15,56 +15,40 @@ public class ParserApp {
     private final Command morphline;
 
     private static void usage() {
-        System.out.println("usage: java ... <morphline.conf> <morphlineId> <dataFile1> ... <dataFileN>");
+        System.out.println("usage: java ... <morphline.conf> <dataFile1> ... <dataFileN>");
         System.exit(1);
     }
 
-    public ParserApp(File morphlineFile, String morphlineId) {
+    public ParserApp(File morphlineFile) {
         this.morphlineContext = new MorphlineContext.Builder().build();
-        this.morphline = new Compiler().compile(morphlineFile, morphlineId, morphlineContext, null);
+        this.morphline = new Compiler().compile(morphlineFile, null, morphlineContext, null);
     }
 
     public boolean[] process(String[] inputs) throws IOException {
         boolean[] outcome = new boolean[inputs.length];
         // Process each input data file
         Notifications.notifyBeginTransaction(morphline);
-        try {
-            for (int i = 0; i < inputs.length; i++) {
-                InputStream in = new BufferedInputStream(new FileInputStream(new File(inputs[i])));
-                Record record = new Record();
-                record.put(Fields.ATTACHMENT_BODY, in);
-                Notifications.notifyStartSession(morphline);
-                outcome[i] = morphline.process(record);
-                if (!outcome[i]) {
-                    System.out.println("Morphline failed to process record: " + record + "for file " + inputs[i]);
-                }
-                in.close();
+        for (int i = 0; i < inputs.length; i++) {
+            InputStream in = new BufferedInputStream(new FileInputStream(new File(inputs[i])));
+            Record record = new Record();
+            record.put(Fields.ATTACHMENT_BODY, in);
+            Notifications.notifyStartSession(morphline);
+            outcome[i] = morphline.process(record);
+            if (!outcome[i]) {
+                System.out.println("Morphline failed to process record: " + record + "for file " + inputs[i]);
             }
-            Notifications.notifyCommitTransaction(morphline);
-        } catch (RuntimeException e) {
-            Notifications.notifyRollbackTransaction(morphline);
-            morphlineContext.getExceptionHandler().handleException(e, null);
-        } catch (IOException e) {
-            Notifications.notifyRollbackTransaction(morphline);
-            throw e;
+            in.close();
         }
         Notifications.notifyShutdown(morphline);
         return outcome;
     }
 
     public static void main(String[] args) throws IOException {
-        if (args.length < 3) {
+        if (args.length < 2) {
             usage();
         }
 
-        File morphlineFile = new File(args[0]);
-        if (!morphlineFile.canRead() || !morphlineFile.exists()) {
-            System.err.println("unable to read " + args[0]);
-            System.exit(-1);
-        }
-
-        String morphlineId = args[1];
-        ParserApp myMorphlineDriver = new ParserApp(morphlineFile, morphlineId);
-        boolean[] outcome = myMorphlineDriver.process(Arrays.copyOfRange(args, 2, args.length));
+        ParserApp driver = new ParserApp(new File(args[0]));
+        boolean[] outcome = driver.process(Arrays.copyOfRange(args, 2, args.length));
     }
 }
