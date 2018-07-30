574b427dadd5427d5b7387c76d9634411c27927f
Vic Hargrave <vichargrave@gmailcom>
2018-07-30 10:40:27 -0700

    All methods throw Exception

--- basicparser/src/test/java/io/github/vichargrave/basicparser/BasicParserTest.java
+++ basicparser/src/test/java/io/github/vichargrave/basicparser/BasicParserTest.java
@@ -5,26 +5,25 @@ import org.kitesdk.morphline.api.MorphlineCompilationException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 
 import static org.junit.Assert.assertTrue;
 
 public class BasicParserTest {
 
     @Test
-    public void testBasicParser() throws IOException {
+    public void testBasicParser() throws Exception {
         ParserApp app = new ParserApp(new File("../conf/parsers.conf"));
         assertTrue(app.process(new String[] {"../data/tweets.json"})[0]);
     }
 
     @Test (expected = MorphlineCompilationException.class)
-    public void testBasicParserNonexistentMorphlineFile() throws IOException {
+    public void testBasicParserNonexistentMorphlineFile() throws Exception {
         ParserApp app = new ParserApp(new File("../conf/_some_file_that_does_not_exist.conf"));
         app.process(new String[] {"../data/tweets.json"});
     }
 
     @Test (expected = FileNotFoundException.class)
-    public void testBasicParserAppNonExistentDataFile() throws IOException {
+    public void testBasicParserAppNonExistentDataFile() throws Exception {
         ParserApp app = new ParserApp( new File("../conf/parsers.conf"));
         app.process(new String[] {"../data/_some_file_that_does_not_exist.json"});
     }
